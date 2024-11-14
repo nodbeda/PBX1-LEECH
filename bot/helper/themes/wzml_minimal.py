c9b20d8fe8300c7b0005e6b8e3f393afd568d1e1
NF Owner  Owner DF <bnfowner@gmail.com>
2024-11-14 02:13:19 +0000

    Update wzml_minimal.py

--- bot/helper/themes/wzml_minimal.py
+++ bot/helper/themes/wzml_minimal.py
@@ -2,10 +2,10 @@
 class WZMLStyle:
     # ----------------------
     # async def start(client, message) ---> __main__.py
-    ST_BN1_NAME = 'Repo'
-    ST_BN1_URL = 'https://www.google.com'
-    ST_BN2_NAME = 'Updates'
-    ST_BN2_URL = 'https://t.me/PBX1_BOTS'
+    ST_BN1_NAME = 'Updates'
+    ST_BN1_URL = 'https://www.google.co.uk/'
+    ST_BN2_NAME = 'Owner⭐'
+    ST_BN2_URL = 'https://t.me/@Owner_BNF'
     ST_MSG = '''<i>This bot can mirror all your links|files|torrents to Google Drive or any rclone cloud or to telegram or to ddl servers.</i>
 <b>Type {help_command} to get a list of available commands</b>'''
     ST_BOTPM = '''<i>Now, This bot will send all your files and links here. Start Using ...</i>'''
